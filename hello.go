f3c82fb6b4e141bc5cd982ed39b1026010ae6d9a
Josue Freitas <josue.freitas@concrete.com.br>
2018-02-09 09:18:02 -0200

    Estudando GoLang

--- hello.go
+++ hello.go
@@ -2,24 +2,47 @@ package main
 
 import "fmt"
 import "os"
+import "net/http"
 
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
+	for {
+		exibeMenu()
+		comando := leComando()
+		switch comando {
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs")
+		default:
+			fmt.Println("Opção inválida")
+			os.Exit(-1)
+		}
+	}
+}
 
-	switch comando {
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	case 1:
-		fmt.Println("Monitorando o sistema...")
-	case 2:
-		fmt.Println("Exibindo logs")
-	default:
-		fmt.Println("Opção inválida")
-		os.Exit(-1)
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando o sistema...")
+	sites := []string{
+		"https://random-status-code.herokuapp.com",
+		"https://www.alura.com.br",
+		"https://www.caelum.com.br"}
+
+	for _, site := range sites {
+		testaSite(site)
+	}
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "Resposta:", resp.StatusCode)
+	} else {
+		fmt.Println("Site:", site, "esta com problema:", resp.StatusCode)
 	}
 }
 
@@ -39,6 +62,6 @@ func exibeMenu() {
 func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
-	fmt.Println("O comando escolhido foi:", &comandoLido)
+	fmt.Println("O comando escolhido foi:", comandoLido)
 	return comandoLido
 }
