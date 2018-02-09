a9af9f6fb024489b595cbcb21eb9153ae52a0494
Josue Freitas <josue.freitas@concrete.com.br>
2018-02-09 11:09:14 -0200

    Lendo dados de arquivo

--- hello.go
+++ hello.go
@@ -1,15 +1,19 @@
 package main
 
-import "fmt"
-import "os"
-import "net/http"
-import "time"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
 
 const monitoramentos = 3
 const delay = 2
 
 func main() {
-
 	exibeIntroducao()
 	for {
 		exibeMenu()
@@ -29,13 +33,32 @@ func main() {
 	}
 }
 
+func leSitesDoArquivo() []string {
+	var sites []string
+	// arquivo, err := os.Open("sites.txt")
+	// arquivo, err := ioutil.ReadFile("sites.txt")
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("[Erro] Motivo:", err)
+	} else {
+		leitor := bufio.NewReader(arquivo)
+		for {
+			linha, err := leitor.ReadString('\n')
+			linha = strings.TrimSpace(linha)
+			sites = append(sites, linha)
+			fmt.Println("Linha adicionada:", linha)
+			if err == io.EOF {
+				break
+			}
+		}
+	}
+	return sites
+}
+
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando o sistema...")
-	sites := []string{
-		"https://random-status-code.herokuapp.com",
-		"https://www.alura.com.br",
-		"https://www.caelum.com.br"}
-
+	sites := leSitesDoArquivo()
 	for i := 0; i < monitoramentos; i++ {
 		for _, site := range sites {
 			testaSite(site)
@@ -43,15 +66,18 @@ func iniciarMonitoramento() {
 		fmt.Println("-------------------------------------")
 		time.Sleep(delay * time.Second)
 	}
-
 }
 
 func testaSite(site string) {
-	resp, _ := http.Get(site)
-	if resp.StatusCode == 200 {
-		fmt.Println("Site:", site, "Resposta:", resp.StatusCode)
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Println("[ERRO] Motivo: ", err)
 	} else {
-		fmt.Println("Site:", site, "esta com problema:", resp.StatusCode)
+		if resp.StatusCode == 200 {
+			fmt.Println("Site:", site, "Resposta:", resp.StatusCode)
+		} else {
+			fmt.Println("Site:", site, "esta com problema:", resp.StatusCode)
+		}
 	}
 }
 
@@ -59,7 +85,7 @@ func exibeIntroducao() {
 	nome := "Gopher"
 	var versao float32 = 1.0
 	fmt.Println("Ola, sr.", nome)
-	fmt.Println("Versao: ", versao)
+	fmt.Println("Versão:", versao)
 }
 
 func exibeMenu() {
@@ -71,6 +97,5 @@ func exibeMenu() {
 func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
-	fmt.Println("O comando escolhido foi:", comandoLido)
 	return comandoLido
 }
